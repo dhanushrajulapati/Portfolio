bebb4572d8c693b55ac4b810753502298d879c3a
dhanushrajulapati <dhanushrajulapati246@gmail.com>
2025-01-09 23:24:16 +0530

    Hello!

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+
+
+const App = () => {
+  return (
+    <div className='3xl justify-center text-center text-red-400 p-20 mt-2 '>
+      <h1  >Hello ! </h1>
+      <a href ="https://www.instagram.com" ><FaInstagram/></a>
+    </div>
+  )
+}
+
+export default App
